f3d0f993ed95f2956c297e9ebd959dd67298a16c
baekkayoung <baekkayoung@naver.com>
2025-06-26 18:08:33 +0900

    과제

--- 01_Variable_Pratice/src/com/kh/pratice/func/VariablePratice4.java
+++ 01_Variable_Pratice/src/com/kh/pratice/func/VariablePratice4.java
@@ -1,8 +1,23 @@
 package com.kh.pratice.func;
 
+import java.util.Scanner;
+
 public class VariablePratice4 {
 
-	public static void main(String[] args) {
+	public void inputTest4(){
+		
+		Scanner sc = new Scanner(System.in);
+		/*
+		 영어 문자열 값을 키보드로 입력 받아 문자에서 앞에서 세 개를 출력하세요.
+		 */
+		
+		System.out.print("문자열을 입력하세요 : ");
+		String name = sc.nextLine();
+		
+		System.out.println("첫 번째 문자 : " + name.charAt(0));
+		System.out.println("두 번째 문자 : " + name.charAt(1));
+		System.out.println("세 번째 문자 : " + name.charAt(2));
+		
 		
 
 	}
--- 01_Variable_Pratice/src/com/kh/pratice/run/Run.java
+++ 01_Variable_Pratice/src/com/kh/pratice/run/Run.java
@@ -3,6 +3,7 @@ package com.kh.pratice.run;
 import com.kh.pratice.func.VariablePratice1;
 import com.kh.pratice.func.VariablePratice2;
 import com.kh.pratice.func.VariablePratice3;
+import com.kh.pratice.func.VariablePratice4;
 
 public class Run {
 
@@ -15,7 +16,11 @@ public class Run {
 		//b.inputTest2();
 				
 		VariablePratice3 c = new VariablePratice3();
-		c.inputTest3();
+		//c.inputTest3();
+		
+		VariablePratice4 d = new VariablePratice4();
+		d.inputTest4();
+		
 	
 
 	}
